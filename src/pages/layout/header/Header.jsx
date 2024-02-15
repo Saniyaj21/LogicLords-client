73b260fa03c9c0b56fe7db4b85fdd840d2b04c8b
Pritam <123.paulpritam@gmail.com>
2024-02-15 14:25:48 +0530

    header

--- src/pages/layout/header/Header.jsx
+++ src/pages/layout/header/Header.jsx
@@ -1,7 +1,30 @@
 import "./header.scss";
+import logo from "../../../media/Logo.png";
+// import namedLogo from "../../../media/TextLogo.png";
 
 const Header = () => {
-	return <div id='header'>Header</div>;
+    return (
+        <div id="header">
+            <div className="logo">
+                <img src={logo} alt="logiclords.dev" />
+                {/* <img src={namedLogo} alt="logiclords.dev" /> */}
+            </div>
+            <div className="links">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Courses</li>
+                    <li>Notes</li>
+                    <li>Leaderboard</li>
+                </ul>
+                <ul className="authLinks">
+                    <li>Login</li>
+                    <li>Register</li>
+                    <li>Logout</li>
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Header;
