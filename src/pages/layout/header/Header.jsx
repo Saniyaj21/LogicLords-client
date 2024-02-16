38b8c940e833191cff4bd8eb839eaa07cd60c004
Pritam <123.paulpritam@gmail.com>
2024-02-16 18:27:24 +0530

    header

--- src/pages/layout/header/Header.jsx
+++ src/pages/layout/header/Header.jsx
@@ -1,21 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaHome, FaProjectDiagram, FaDiscourse } from "react-icons/fa";
+import { MdLeaderboard } from "react-icons/md";
+import Logo from "../../../media/Logo1.png";
 import "./header.scss";
-import Logo from "../../../components/Logo";
 
 const Header = () => {
     return (
         <header id="header">
-            <div id="header-logo">
-                <Logo />
+            <div className="logoContainer">
+                <img src={Logo} alt="logiclords.dev" />
+                <h1 className="nunito-8">
+                    Logiclords<span className="nunito-4">.dev</span>
+                </h1>
             </div>
-            <div id="menu">
-                <ul>
-                    <li>Home</li>
-                    <li>Courses</li>
-                    <li>Projects</li>
-                    <li>Leader Board</li>
-                </ul>
-            </div>
-            <div id="hamburg">HAM</div>
+            <ul className="linksContainer">
+                <Link to="/" className="currentLink">
+                    <li>
+                        <FaHome className="icons" /> Home
+                    </li>
+                </Link>
+                <Link to="/">
+                    <li>
+                        <FaDiscourse className="icons" /> Courses
+                    </li>
+                </Link>
+                <Link to="/">
+                    <li>
+                        <FaProjectDiagram className="icons" />
+                        Projects
+                    </li>
+                </Link>
+                <Link to="/">
+                    <li>
+                        <MdLeaderboard className="icons" /> Leader Board
+                    </li>
+                </Link>
+
+                <div className="authContainer">
+                    <Link to="/auth/login" className="loginBtn">
+                        Login
+                    </Link>
+                    <Link to="/auth/register">Register</Link>
+                </div>
+            </ul>
         </header>
     );
 };
