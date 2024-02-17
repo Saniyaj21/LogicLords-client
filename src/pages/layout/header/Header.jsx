ec09a50d3c8066043c1536ea263dc0e75addd5f3
saniyaj mallik <saniyajmallik12@gmail.com>
2024-02-17 09:07:25 +0530

    icon removed

--- src/pages/layout/header/Header.jsx
+++ src/pages/layout/header/Header.jsx
@@ -2,50 +2,50 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaHome, FaProjectDiagram, FaDiscourse } from "react-icons/fa";
 import { MdLeaderboard } from "react-icons/md";
-import Logo from "../../../media/Logo1.png";
+import Logo from "../../../media/logo1.png";
 import "./header.scss";
 
 const Header = () => {
-    return (
-        <header id="header">
-            <div className="logoContainer">
-                <img src={Logo} alt="logiclords.dev" />
-                <h1 className="nunito-8">
-                    Logiclords<span className="nunito-4">.dev</span>
-                </h1>
-            </div>
-            <ul className="linksContainer">
-                <Link to="/" className="currentLink">
-                    <li>
-                        <FaHome className="icons" /> Home
-                    </li>
-                </Link>
-                <Link to="/">
-                    <li>
-                        <FaDiscourse className="icons" /> Courses
-                    </li>
-                </Link>
-                <Link to="/">
-                    <li>
-                        <FaProjectDiagram className="icons" />
-                        Projects
-                    </li>
-                </Link>
-                <Link to="/">
-                    <li>
-                        <MdLeaderboard className="icons" /> Leader Board
-                    </li>
-                </Link>
+	return (
+		<header id='header'>
+			<div className='logoContainer'>
+				
+					<img src={Logo} alt='logiclords.dev' />
+				
+				<h1 className='nunito-8'>
+					Logiclords<span className='nunito-4'>.dev</span>
+				</h1>
+			</div>
+			<ul className='linksContainer'>
+				<Link to='/' className='currentLink'>
+					<li>
+						Home
+					</li>
+				</Link>
+				<Link to='/'>
+					<li>
+						Courses
+					</li>
+				</Link>
+				<Link to='/'>
+					<li>
+						Projects
+					</li>
+				</Link>
+				<Link to='/'>
+					<li>
+						Leader Board
+					</li>
+				</Link>
 
-                <div className="authContainer">
-                    <Link to="/auth/login" className="btn">
-                        Login
-                    </Link>
-                    {/* <Link to="/auth/register">Register</Link> */}
-                </div>
-            </ul>
-        </header>
-    );
+				<div className='authContainer'>
+					<Link to='/auth/login'>
+						<button className='btn'>Join us</button>
+					</Link>
+				</div>
+			</ul>
+		</header>
+	);
 };
 
 export default Header;
