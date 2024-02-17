fb1e699aeb3c1af2cb00fc1097b6095122d8b382
Pritam <123.paulpritam@gmail.com>
2024-02-17 14:26:52 +0530

    Header Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/Home";
 import Header from "./pages/layout/header/Header";
+import Home from "./pages/home/Home";
+import Courses from "./pages/courses/Courses";
+import Projects from "./pages/projects/Projects";
 import Footer from "./pages/layout/footer/Footer";
 import ErrorPage from "./pages/error/ErrorPage";
 
@@ -10,8 +12,8 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} exact />
-                <Route path="/courses" element={<Home />} />
-                <Route path="/projects" element={<Home />} />
+                <Route path="/courses" element={<Courses />} />
+                <Route path="/projects" element={<Projects />} />
                 <Route path="/leader-board" element={<Home />} />
                 <Route path="/*" element={<ErrorPage />} />
             </Routes>
---
+++ src/pages/courses/Courses.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./courses.scss";
+
+const Courses = () => {
+    return <div>Courses</div>;
+};
+
+export default Courses;
