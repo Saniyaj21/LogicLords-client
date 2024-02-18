a0919aa797f3b3510467bc56d8e01486e6aea2a4
shuvra442 <shuvrapatra5@gmail.com>
2024-02-18 18:24:48 +0530

    sh3 complete leader board

--- src/pages/leader_Board/leader_Board_Layout/GlobalRank.jsx
+++ src/pages/leader_Board/leader_Board_Layout/GlobalRank.jsx
@@ -2,27 +2,29 @@ import React from "react";
 import "./GlobalRank.scss";
 
 // img add
-import Image from '../../../media/images.png'
-import Logo from '../../../media/Logo.png'
+import Image from "../../../media/images.png";
+import Logo from "../../../media/Logo.png";
 
 export default function GlobalRank() {
   return (
     <main>
       <div className="leaderBoard_mainDiv">
-        <span className="User_SN">1.</span>
-        <img src={Image} alt="User_Img" className="User_Img" />
-        
+        <div className="User_SN">1.</div>
         <div className="User_Info">
-          <span className="user_name">Shuvra patra</span>
-          <span className="Inst_Name">Institute name</span>
-        </div>
-        
-        {/* </span> */}
-        <img src={Logo} alt="LL_Logic" className="LL_logo" />
-        <span className="user_coin">1200</span>
-      </div>
+          <img src={Image} alt="User_Img" className="User_Img" />
 
-      
+          <div className="info">
+            <div className="user_name">Shuvra patra</div>
+            <div className="Inst_Name">Institute name</div>
+          </div>
+        </div>
+
+        
+        <div className="Rank_info">
+          <img src={Logo} alt="LL_Logic" className="LL_logo" />
+          <span className="user_coin">1200</span>
+        </div>
+      </div>
     </main>
   );
 }
