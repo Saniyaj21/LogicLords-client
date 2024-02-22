d2ec5d6e2a32b3349f5a64650ae62d3e1200251f
Sombhu2022 <sombhudas93@gmail.com>
2024-02-22 10:53:20 +0530

    course page fix

--- src/pages/courses/components/CourseComponent.jsx
+++ src/pages/courses/components/CourseComponent.jsx
@@ -10,71 +10,81 @@ import { BsArrowRight } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 function CourseComponent({
-	thumbnail,
-	courseType,
-	courseDescription,
-	instractorDp,
-	instractorName,
-	ratting = "",
-	totalRattingAmmount = "",
-	isHistory = false,
+  thumbnail,
+  courseType,
+  courseDescription,
+  instractorDp,
+  instractorName,
+  instractorRole,
+  ratting ,
+  totalRattingAmmount ,
+  isHistory = false,
+  isFree=true,
+  progress
 }) {
-	useEffect(() => {}, []);
+  useEffect(() => {}, []);
 
- 
+  return (
+    <div className="course-cart">
+      <div className="video-container">
+        <img src={courseImg} alt="" />
+      </div>
 
-	return (
-		<div className='course-cart'>
-			<div className='video-container'>
-				
-				<img src={courseImg} alt='' />
-			</div>
+      <div className="course-info">
+        <span className="course-type">{courseType}</span>
+        <p className="course-description nunito-6">
+         {courseDescription}
+        </p>
+      </div>
 
-			<div className='course-info'>
-				<span className='course-type'>Fontend</span>
-				<p className='course-description nunito-6'>
-					Dignissimos ad necessitatibus delectus nisi adipisci!
-				</p>
-			</div>
+      {isHistory ? <div className="progress-container" >
+        <div className="progress" style={{width: progress}}></div>
+      </div> : ""}
 
-			{isHistory ? <div className='progress' id='progress'></div> : ""}
+      <div className="instractor-info">
+        <div className="dp-container">
+          <img src={dp} alt="" />
+        </div>
+        <div className="other-info">
+          <p className="name">{instractorName}</p>
+          <p className="role">{instractorRole}</p>
+        </div>
+      </div>
 
-			<div className='instractor-info'>
-				<div className='dp-container'>
-					<img src={dp} alt='' />
-				</div>
-				<div className='other-info'>
-					<p className='name'  >Sombhu Das</p>
-					<p className='role'>Web developer</p>
-				</div>
-			</div>
+      {isHistory ? (
+        ""
+      ) : (
+        <div className="course-other-info" id="course-other-info">
+          <p>{isFree? "Free":"Paid"}</p>
 
-			<div className='course-other-info' id='course-other-info'>
-				<p>Free</p>
-				<div className='ratting'>
-					<div>
-						<p className='nunito-6'>5 </p>
-						<p className='star-container'>
-							<Rate count={5} value={5} allowHalf disabled />
-						</p>
-					</div>
-					<div>
-						<p className='nunito-6 ammount-of-student'>(900)</p>
-					</div>
-				</div>
-			</div>
+          {
+            ratting? (<div className="ratting">
+            <div>
+              <p className="nunito-6">{ratting}</p>
+              <p className="star-container">
+                <Rate count={5} value={ratting} allowHalf disabled />
+              </p>
+            </div>
+            <div>
+              <p className="nunito-6 ammount-of-student">({totalRattingAmmount})</p>
+            </div>
+          </div>):("")
+          }
+          
+        </div>
+      )}
 
-			<div className='enroal-container'>
-				<Link 
+      <div className="enroal-container">
+        <Link
         // to={"/playlist/id"}
         >
-					<button className='btn'>
-						Enrole Now <BsArrowRight />
-					</button>
-				</Link>
-			</div>
-		</div>
-	);
+          <button className="btn">
+            {isHistory ? "Continue" : "Enrole Now"} <BsArrowRight />
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default CourseComponent;
