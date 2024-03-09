365ab2080e9194d498f7f211a4f9328b075367eb
saniyaj mallik <saniyajmallik12@gmail.com>
2024-03-09 17:07:14 +0530

    vercel backend transformation

--- src/main.jsx
+++ src/main.jsx
@@ -26,4 +26,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 
 
 // in production code push this
- export const base_url = 'https://logiclords-server.onrender.com';
+ export const base_url = 'https://logiclords-server.vercel.app';
